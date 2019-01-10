b7fd20aacbcf1ca07803a5e7f6c9801b8640bf2c
Hassen Taidirt <htaidirt@gmail.com>
2019-01-10 14:47:01 +0100

    Add element to SQS queue

--- src/upload.ts
+++ src/upload.ts
@@ -1,26 +1,32 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import * as AdmZip from 'adm-zip';
-import { S3 } from 'aws-sdk';
-import { extractS3Params } from '../lib/events';
-import { success, failure } from '../lib/response';
+import * as path from 'path'
+import * as fs from 'fs'
+import * as AdmZip from 'adm-zip'
+import { S3, SQS } from 'aws-sdk'
+import { extractS3Params } from '../lib/events'
+import { success, failure } from '../lib/response'
 
-const s3Client = new S3();
+const s3Client = new S3()
+const sqsClient = new SQS()
+
+const queueURL: string = `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${
+  process.env.AWS_ACCOUNT_ID
+}/${process.env.AWS_QUEUE_NAME}`
 
 export const run: any = async (event: any) => {
-  const s3Object = extractS3Params(event);
+  const s3Object = extractS3Params(event)
+  let sqsMessage, sqsSendResponse: SQS.SendMessageResult
 
   try {
     // Get remote S3 document (pli)
     const s3Pli: S3.GetObjectOutput = await s3Client
       .getObject(s3Object)
-      .promise();
+      .promise()
 
     // Save document locally for unzip
-    const tmpZipFile = path.join(process.env.PWD, 'tmp', s3Object.Key);
-    fs.writeFileSync(tmpZipFile, s3Pli.Body);
+    const tmpZipFile = path.join(process.env.PWD, 'tmp', s3Object.Key)
+    fs.writeFileSync(tmpZipFile, s3Pli.Body)
 
-    // Upload to S3 the document inside the zip
+    // Upload to S3 the documents inside the zip
     await Promise.all(
       new AdmZip(tmpZipFile)
         .getEntries()
@@ -34,13 +40,26 @@ export const run: any = async (event: any) => {
             })
             .promise()
         )
-    );
+    )
+
+    // Push new job in the SQS queue for processing
+    sqsMessage = { folder: s3Object.Key }
+    sqsSendResponse = await sqsClient
+      .sendMessage({
+        QueueUrl: queueURL,
+        MessageBody: JSON.stringify(sqsMessage)
+      })
+      .promise()
   } catch (error) {
-    return failure(error);
+    return failure(error)
   }
 
   return success({
-    message: 'Hey, TS is awesome',
-    input: event
-  });
-};
+    message:
+      'Processing new upload finished and ready to be processed by OCR module',
+    event,
+    s3OriginalObject: s3Object,
+    sqsMessage,
+    sqsSendResponse
+  })
+}
